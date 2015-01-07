89a6eea5ed559f4ec405ab488ce734d8c1595de3
ClaudiusJ <ClaudiusJ@live.de>
2015-01-07 19:49:31 +0100

    Update version info: 0.7.3 Edda

--- EScript/Version.h
+++ EScript/Version.h
@@ -1,7 +1,7 @@
 // Version.h
 // This file is part of the EScript programming language (https://github.com/EScript)
 //
-// Copyright (C) 2012-2014 Claudius Jähn <ClaudiusJ@live.de>
+// Copyright (C) 2012-2015 Claudius Jähn <ClaudiusJ@live.de>
 // Copyright (C) 2012 Benjamin Eikel <benjamin@eikel.org>
 //
 // Licensed under the MIT License. See LICENSE file for details.
@@ -9,12 +9,12 @@
 #ifndef ES_VERSION_H
 
 // huge(always 0) * 10000 + major * 100 + minor
-#define ES_VERSION 702
-#define ES_VERSION_STRING "EScript 0.7.2 Egon (https://github.com/EScript)\0"
-#define ES_VERSION_NAME "Egon\0"
+#define ES_VERSION 703
+#define ES_VERSION_STRING "EScript 0.7.3 Edda (https://github.com/EScript)\0"
+#define ES_VERSION_NAME "Edda\0"
 
 // used for the windows resources file:
-#define ES_VERSION_RES 0,7,2,0
-#define ES_VERSION_RES_STRING "0,7,2,0\0"
+#define ES_VERSION_RES 0,7,3,0
+#define ES_VERSION_RES_STRING "0,7,3,0\0"
 
 #endif // ES_VERSION_H
--- tests/test.escript
+++ tests/test.escript
@@ -1,7 +1,7 @@
 // test.escript
 // This file is part of the EScript programming language (https://github.com/EScript)
 //
-// Copyright (C) 2011-2013 Claudius Jähn <ClaudiusJ@live.de>
+// Copyright (C) 2011-2015 Claudius Jähn <ClaudiusJ@live.de>
 // Copyright (C) 2012 Benjamin Eikel <benjamin@eikel.org>
 //
 // Licensed under the MIT License. See LICENSE file for details.
