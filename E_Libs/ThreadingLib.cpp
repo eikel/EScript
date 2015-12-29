ccb1522d43f9b997c0a336b66074b2f4a2c3063c
ClaudiusJ <ClaudiusJ@live.de>
2015-12-29 21:28:10 +0100

    Join thread and stop the runtime when thread is destroyed. Add Mutex and LockGuard type.

--- E_Libs/ThreadingLib.cpp
+++ E_Libs/ThreadingLib.cpp
@@ -9,18 +9,20 @@
 #if defined(ES_THREADING)
 
 #include "../EScript/EScript.h"
+#include "../EScript/Objects/ReferenceObject.h"
 #include "ThreadingLib.h"
 
 #include <iostream>
 #include <thread>
 #include <mutex>
 
+
 namespace EScript{
 
 
 class E_Thread : public Object{
 		ES_PROVIDES_TYPE_NAME(Thread)
-	public: 
+	public:
 		//! (static)
 		static Type * getTypeObject(){
 			static Type * typeObject = new Type(Object::getTypeObject()); // ---|> Object
@@ -30,86 +32,105 @@ class E_Thread : public Object{
 		ERef<Runtime> rt;
 		ObjRef fun;
 		std::thread thread;
-		
-		E_Thread(ERef<Runtime> _rt,ObjRef _fun,Type* type=nullptr) : 
-				Object(type?type:getTypeObject()), 
+
+		E_Thread(ERef<Runtime> _rt,ObjRef _fun,Type* type=nullptr) :
+				Object(type?type:getTypeObject()),
 				rt(std::move(_rt)), fun(std::move(_fun)){}
-		virtual ~E_Thread(){}
+		virtual ~E_Thread(){
+			std::cout << "~Thread..." << std::endl;
+			if(rt)
+				rt->_setExitState(nullptr);
+			if(thread.joinable())
+				thread.join();
+			std::cout << "~Thread." << std::endl;
+		}
 
 		void run(){
 			thread = std::move(std::thread([](E_Thread* eThread){
 				eThread->rt->executeFunction( eThread->fun, nullptr, ParameterValues() );
 			},this));
 		}
+		void join(){
+			std::cout << "join..." << std::endl;
+			thread.join();
+			std::cout << "join." << std::endl;
+		}
+
 };
 
+class E_Mutex : public ReferenceObject<std::mutex,Policies::SameEObjects_ComparePolicy>{
+    ES_PROVIDES_TYPE_NAME(Mutex)
+public:
+    static Type * getTypeObject(){
+        static Type * typeObject = new Type(Object::getTypeObject()); // ---|> Object
+        return typeObject;
+    }
+    E_Mutex() : ReferenceObject(getTypeObject()){}
+    virtual ~E_Mutex(){}
+};
 
+class E_LockGuard : public ReferenceObject<std::unique_lock<std::mutex>,Policies::SameEObjects_ComparePolicy>{
+    ES_PROVIDES_TYPE_NAME(LockGuard)
+public:
+    static Type * getTypeObject(){
+        static Type * typeObject = new Type(Object::getTypeObject()); // ---|> Object
+        return typeObject;
+    }
+    E_LockGuard(std::mutex& inMutex) : ReferenceObject(getTypeObject(),inMutex){}
+    virtual ~E_LockGuard(){}
+};
+}
+
+ES_CONV_EOBJ_TO_OBJ(E_Mutex, std::mutex*, &**eObj)
+ES_CONV_EOBJ_TO_OBJ(E_LockGuard, std::unique_lock<std::mutex>*, &**eObj)
+
+namespace EScript{
 //! (static)
 void ThreadingLib::init(EScript::Namespace * globals) {
 	Namespace * lib = new Namespace;
 	declareConstant(globals,"Threading",lib);
-	
+
 	{ // thread
 		declareConstant(lib, E_Thread::getClassName(), E_Thread::getTypeObject() );
-		
-	}
-	
 
-//	struct Starter{
-//		ERef<Runtime rt> _t;
-//		ObjRef fun;
-//		Starter(ERef<Runtime rt> _rt,ObjRef _fun) : rt(std::move(_rt)),fun(std::move(_fun)){}
-//		void operator(){
-//			
-//		}
-//	};
-	
+        //! [ESMF]	self Thread.join(  )
+        ES_MFUN(E_Thread::getTypeObject(),E_Thread,"join",0,0,(thisObj->join(),thisEObj))
+
+        // isJoinable
+        // kill
+	}
+	{ // mutex
+		declareConstant(lib, E_Mutex::getClassName(), E_Mutex::getTypeObject() );
+
+        ES_CTOR(E_Mutex::getTypeObject(), 0, 0, new E_Mutex); // Value::create<E_Mutex>()
+
+        //! [ESMF]	self Mutex.lock(  )
+        ES_MFUN(E_Mutex::getTypeObject(), std::mutex, "lock", 0, 0,( thisObj->lock(),thisEObj))
+
+        //! [ESMF]	self Mutex.unlock(  )
+        ES_MFUN(E_Mutex::getTypeObject(), std::mutex, "unlock", 0, 0,( thisObj->lock(),thisEObj))
+
+        //! [ESMF]	bool Mutex.tryLock(  )
+        ES_MFUN(E_Mutex::getTypeObject(), std::mutex, "tryLock", 0, 0, thisObj->try_lock())
+	}
+
+	{ // lockGuard
+		declareConstant(lib, E_LockGuard::getClassName(), E_LockGuard::getTypeObject() );
+        ES_CTOR(E_LockGuard::getTypeObject(), 1, 1, new E_LockGuard(*parameter[0].to<std::mutex*>(rt)))
+	}
 	//! [ESF]	void run( fn )
 	ES_FUNCTION(lib,"run",1,1,{
-		E_Thread* eThread = new E_Thread( std::move(rt._fork()),parameter[0] );
-		eThread->run();
-		return eThread;
+		ObjRef eThread(new E_Thread( std::move(rt._fork()),parameter[0] ));
+		static_cast<E_Thread*>(eThread.get())->run();
+		return std::move(eThread);
 	})
-//	
-//	//!	[ESF] ExtObject new ExtObject( [Map objAttributes] )
-//	ES_CONSTRUCTOR(typeObject,0,1,{
-//		ERef<ExtObject> result(new ExtObject(thisType));
-//		if(parameter.count()>0){
-//			Map * m = assertType<Map>(rt,parameter[0]);
-//			for(const auto & keyValuePair : *m) {
-//				result->setAttribute(keyValuePair.first, Attribute(keyValuePair.second.value));
-//			}
-//		}
-//		return result.detachAndDecrease();
-//	})
 
-	
-//
-//	//! [ESF]	void setClipboard( string )
-//	ES_FUN(lib,"setClipboard",1,1,(Win32Lib::setClipboard(parameter[0].toString()),RtValue(nullptr)))
-//
-//	//! [ESF]	string getClipboard( )
-//	ES_FUN(lib,"getClipboard",0,0,Win32Lib::getClipboard())
-//
-//	//! [ESF]	bool loadLibrary(string )
-//	ES_FUNCTION(lib,"loadLibrary",1,1, {
-//		HINSTANCE hDLL;
-//		libInitFunction *  f;	// Function pointer
-//
-//		hDLL = LoadLibrary(parameter[0].toString().c_str());
-//		if(hDLL == nullptr)
-//			return false;
-//
-//		f = reinterpret_cast<libInitFunction*>(GetProcAddress(hDLL,"init"));
-//		if(!f)	{
-//			// handle the error
-//			FreeLibrary(hDLL);
-//			return false;
-//		}
-//		EScript::initLibrary(f);
-//
-//		return true;
-//	})
+
+ // lock
+ // lockGuard
+ // tryLock
+ // Mutex
+ // kill all threads on exit!
 
 }
 
