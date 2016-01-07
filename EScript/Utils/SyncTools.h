3a9c2313f04ef18261d5c8936a5724150e614eeb
ClaudiusJ <ClaudiusJ@live.de>
2016-01-07 20:42:08 +0100

    Fix SpinLock and add tryLock.

--- EScript/Utils/SyncTools.h
+++ EScript/Utils/SyncTools.h
@@ -17,12 +17,11 @@ namespace SyncTools{
 namespace _Internals{
 //! \see http://en.cppreference.com/w/cpp/atomic/atomic_flag
 class SpinLock{
-	private:
 		std::atomic_flag f;
 	public:
 		SpinLock():f(ATOMIC_FLAG_INIT){}
 
-		void lock()		{	while(!f.test_and_set(std::memory_order_acquire));	}
+		void lock()		{	while(f.test_and_set(std::memory_order_acquire));	}
 		bool try_lock()	{	return !f.test_and_set(std::memory_order_acquire);	}
 		void unlock()	{	f.clear(std::memory_order_release);		}
 };
@@ -32,7 +31,12 @@ typedef std::atomic<int> atomicInt;
 typedef std::atomic<bool> atomicBool;
 //typedef std::mutex  FastLock;
 typedef _Internals::SpinLock FastLock;
+//typedef std::mutex FastLock;
 typedef std::unique_lock<FastLock> FastLockHolder;
+
+inline SyncTools::FastLockHolder tryLock(FastLock& lock){
+	return std::move(FastLockHolder(lock, std::try_to_lock) );
+}
 
 }
 }
