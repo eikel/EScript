bdd14a8220c864d496b5459fdcebe571cb212dae
Benjamin Eikel <benjamin@eikel.org>
2016-11-05 15:05:04 +0100

    Update copyright

--- EScript/Basics.h
+++ EScript/Basics.h
@@ -2,6 +2,7 @@
 // This file is part of the EScript programming language (https://github.com/EScript)
 //
 // Copyright (C) 2013 Claudius Jähn <ClaudiusJ@live.de>
+// Copyright (C) 2016 Benjamin Eikel <benjamin@eikel.org>
 //
 // Licensed under the MIT License. See LICENSE file for details.
 // ---------------------------------------------------------------------------------
